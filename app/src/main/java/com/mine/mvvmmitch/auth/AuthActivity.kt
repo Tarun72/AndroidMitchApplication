4b77202e51c5943c2cac01c874b1a9dec7ff5ef7
tarun_72 <tarunshrm395@gmail.com>
2021-08-24 19:12:26 +0530

    first commit to master

---
+++ app/src/main/java/com/mine/mvvmmitch/auth/AuthActivity.kt
@@ -0,0 +1,14 @@
+package com.mine.mvvmmitch.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mine.mvvmmitch.R
+
+class AuthActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
