2a301974f29c0447116cfb304208d918d13280c1
tarun_72 <tarunshrm395@gmail.com>
2021-08-27 09:15:34 +0530

    updated the database logics 1. authtokendao 2. accountPersistenceDao 3. AppDatabase

--- app/src/main/java/com/mine/mvvmmitch/openapi/persistance/AccountPersistenceDao.kt
+++ app/src/main/java/com/mine/mvvmmitch/openapi/persistance/AccountPersistenceDao.kt
@@ -1,4 +1,23 @@
 package com.mine.mvvmmitch.openapi.persistance
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mine.mvvmmitch.models.AccountProperties
+
+@Dao
 interface AccountPersistenceDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertOrReplace(accountProperties: AccountProperties):Long
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertOrIgnore(accountProperties: AccountProperties):Long
+
+    @Query("SELECT * FROM account_properties WHERE pk =:pk")
+    fun searchByPK(pk:Int):AccountProperties?
+
+    @Query("SELECT * FROM account_properties WHERE email = :email")
+    fun searchByEmail(email:String):AccountProperties?
 }
--- app/src/main/java/com/mine/mvvmmitch/openapi/persistance/AuthTokenDao.kt
+++ app/src/main/java/com/mine/mvvmmitch/openapi/persistance/AuthTokenDao.kt
@@ -1,4 +1,17 @@
 package com.mine.mvvmmitch.openapi.persistance
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mine.mvvmmitch.models.AuthToken
+
+@Dao
 interface AuthTokenDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertOrReplace(authToken: AuthToken):Long
+
+    @Query("UPDATE auth_token SET token = null WHERE account_pk = :pk")
+    fun nullifyToken(pk:Int):Int
 }
