2330bd9698d11302f41d9ce0bcfe74c1bb1b8bdc
tarun_72 <tarunshrm395@gmail.com>
2021-09-09 09:25:31 +0530

    1. Session is managed

--- app/src/main/java/com/mine/mvvmmitch/session/SessionManager.kt
+++ app/src/main/java/com/mine/mvvmmitch/session/SessionManager.kt
@@ -1,12 +1,102 @@
 package com.mine.mvvmmitch.session
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.mine.mvvmmitch.models.AuthToken
 import com.mine.mvvmmitch.openapi.persistance.AuthTokenDao
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class SessionManager
 @Inject
-constructor(authTokenDao: AuthTokenDao,application: Application) {
+constructor(val authTokenDao: AuthTokenDao,
+            val application: Application) {
+    private val _cachedToken = MutableLiveData<AuthToken>()
+    private val TAG: String = "SessionManager"
+
+    val cacheToken:LiveData<AuthToken>
+    get() = _cachedToken
+
+    fun login(newToken: AuthToken){
+        setValue(newToken)
+    }
+
+    private fun setValue(newToken: AuthToken?) {
+        GlobalScope.launch(Main) {
+            if (_cachedToken.value != newToken) {
+                _cachedToken.value = newToken
+            }
+        }
+    }
+
+    fun logout(){
+        Log.d(TAG, "logout: ")
+
+        CoroutineScope(IO).launch{
+            var errorMessage: String? = null
+            try{
+                _cachedToken.value!!.account_pk?.let { authTokenDao.nullifyToken(it)
+                } ?: throw CancellationException("Token Error. Logging out user.")
+            }catch (e: CancellationException) {
+                Log.e(TAG, "logout: ${e.message}")
+                errorMessage = e.message
+            }
+            catch (e: Exception) {
+                Log.e(TAG, "logout: ${e.message}")
+                errorMessage = errorMessage + "\n" + e.message
+            }
+            finally {
+                errorMessage?.let{
+                    Log.e(TAG, "logout: ${errorMessage}" )
+                }
+                Log.d(TAG, "logout: finally")
+                setValue(null)
+            }
+        }
+    }
+
+    private fun isConnectedToTheInternet(context: Context): Boolean {
+        var result: Boolean = false
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        ConnectivityManager.TYPE_ETHERNET -> true
+                        else -> false
+                    }
+
+                }
+            }
+        }
+
+        return result
+    }
+
 }
